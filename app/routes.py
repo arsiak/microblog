e8a1e8a77e1a043f636d006c4033d189048cefa4
arnaud <arnaud.rosiak@gmail.com>
2018-04-14 11:20:57 +0200

    Correct syntax mistakes

--- app/routes.py
+++ app/routes.py
@@ -97,8 +97,8 @@ def before_request():
 @login_required
 def follow(username):
     user = User.query.filter_by(username=username).first()
-    if user is not None:
-        flash('User {} not found').format(username)
+    if user is None:
+        flash('User {} not found'.format(username))
         return redirect(url_for('index'))
     if user == current_user:
         flash('You cannot follow yourself')
@@ -113,13 +113,13 @@ def follow(username):
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found').format(username)
+        flash('User {} not found'.format(username))
         return redirect(url_for('index'))
     if user == current_user:
         flash("You cannot unfollow yourself")
         return redirect(url_for('user',username=username))
     current_user.unfollow(user)
     db.session.commit()
-    flash('You are not following {}').format(username)
+    flash('You are not following {}'.format(username))
     return redirect(url_for('user', username=username))
 
