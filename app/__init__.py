e6e07f651bcf7162ec3738cfaa5eb67316fc8b24
arnaud <arnaud.rosiak@gmail.com>
2018-04-18 15:12:55 +0200

    inherite base.html from bootstrap set navbar bootstrap v3

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import os
 
 import logging
@@ -20,6 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 from app import routes, models, errors
